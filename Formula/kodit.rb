9d31addbf025d7cd3be294dd2ec61a3c7ef0fadd
Phil Winder <phil@winder.ai>
2025-06-26 14:21:17 +0100

    kodit 0.3.0

--- Formula/kodit.rb
+++ Formula/kodit.rb
@@ -3,8 +3,8 @@ class Kodit < Formula
   
     desc "Kodit is an MCP server that indexes your private codebases"
     homepage "https://docs.helixml.tech/kodit/"
-    url "https://pypi.org/packages/source/k/kodit/kodit-0.2.9.tar.gz"
-    sha256 "900f1de47f94334857082b6265c14062cd49f4cc01dbd9cffe47ed7b1081ff2f"
+    url "https://pypi.org/packages/source/k/kodit/kodit-0.3.0.tar.gz"
+    sha256 "d3d829434be9c538f12804cf3e451885d23593ede5cc32462170ef8976042e35"
     license "Apache-2.0"
   
     depends_on "python@3.13"
